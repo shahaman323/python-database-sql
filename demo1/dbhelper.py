de1d44be4cb1a53961ba86c4e86bfb0893d7ba2b
shah323 <aman@fusemachines.com>
2023-02-13 18:36:00 +0545

    added search function to fetch information of a registered user

--- demo1/dbhelper.py
+++ demo1/dbhelper.py
@@ -5,7 +5,7 @@ class DBhelper:
 
     def __init__(self):
         try:
-            self.conn = mysql.connector.connect(host="localhost", username="root", password="", database="hit-demo-db")
+            self.conn = mysql.connector.connect(host="localhost", user="root", password="", database="hit-demo-db")
             self.mycursor = self.conn.cursor()
         except:
             print("Some error occured. Could not connect to database")
@@ -13,14 +13,23 @@ class DBhelper:
         else:
             print("Connected to Database")
 
-    def register(self,name,email,password):
+    def register(self, name, email, password):
         try:
             self.mycursor.execute("""
             INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, '{}', '{}', '{}');
-            """.format(name,email,password)
-            )
+            """.format(name, email, password)
+                                  )
             self.conn.commit()
         except:
-            return  -1
+            return -1
         else:
             return 1
+
+    def search(self, email, password):
+        self.mycursor.execute("""
+        SELECT * FROM users WHERE email LIKE '{}' AND password LIKE '{}'
+        """.format(email, password))
+
+        data = self.mycursor.fetchall()
+
+        return data
